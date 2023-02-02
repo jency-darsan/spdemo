69926acfb77f65ecc50ba8a8db5950ed03ab0737
Jency <jency@darsantech.com>
2023-02-02 20:37:33 +0530

    Update SpdemoApplication.java

--- src/main/java/com/spdemo/spdemo/SpdemoApplication.java
+++ src/main/java/com/spdemo/spdemo/SpdemoApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpdemoApplication {
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World! Your application is running";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpdemoApplication.class, args);
 	}
