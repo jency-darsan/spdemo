15eeb10308228af0e902ff49656a4b47b25bb5da
Jency <jency@darsantech.com>
2023-02-02 20:33:25 +0530

    Add files via upload

---
+++ src/test/java/com/spdemo/spdemo/SpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spdemo.spdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
